26a67725a60ca407745339ffecf0d788c24e3638
Alexander Kledal <Kledal@users.noreply.github.com>
2018-05-02 08:15:29 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -14,7 +14,7 @@ vali_datagen = ImageDataGenerator(
     cval=0,
 )
 
-model = load_model("model.h5")
+model = load_model("model-a.h5")
 
 #dirs = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 dirs = ['3']
@@ -28,9 +28,17 @@ gen = vali_datagen.flow_from_directory('train_val_symbols/test/',
 )
 
 probas = model.predict_generator(gen)
-y_prob = probas[0]
-max_idx = np.argmax(y_prob)
-print(CHARS[max_idx])
+failed = 0
+
+for i in range(len(probas)):
+    y_prob = probas[i]
+    max_idx = np.argmax(y_prob)
+    predict = CHARS[max_idx]
+    print(predict)
+    if predict != '9':
+        failed = failed +1
+
+print("Total vali: " + str(len(probas)) + ", failed: " + str(failed))
 
 #test_img = vali_datagen.
 #y_prob = model.predict(np.array( [test_img] ))
