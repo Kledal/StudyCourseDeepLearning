8b5d8d541737352c98d4b81a6679acfd726fd057
Alexander Kledal <Kledal@users.noreply.github.com>
2018-05-17 14:15:15 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,31 +6,46 @@ import glob
 
 #from new_model import CHARS
 
-
-CHARS = '0123456789'
+CHARS = '0123456789-ABCDEFGHIJKLMNPQRSTUVXYZ'
 
 vali_datagen = ImageDataGenerator(
     fill_mode='constant',
     cval=0,
 )
 
-model = load_model("model.h5")
+model = load_model("model-all.h5")
 
 #dirs = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-dirs = ['3']
-gen = vali_datagen.flow_from_directory('train_val_symbols/test/',
-    classes=dirs,
-    target_size=(64, 64),
-    color_mode='grayscale',
-    batch_size=1,
-    class_mode=None,
-    shuffle=False
-)
 
-probas = model.predict_generator(gen)
-y_prob = probas[0]
-max_idx = np.argmax(y_prob)
-print(CHARS[max_idx])
+matrix = []
+
+dirs = list(CHARS)
+for folder in dirs:
+  gen = vali_datagen.flow_from_directory('train_val_symbols/val/',
+      classes=[folder],
+      target_size=(64, 64),
+      color_mode='grayscale',
+      batch_size=1,
+      class_mode=None,
+      shuffle=False
+  )
+  
+  probas = model.predict_generator(gen)
+  failed = 0
+  
+  for i in range(len(probas)):
+      y_prob = probas[i]
+      max_idx = np.argmax(y_prob)
+      predict = CHARS[max_idx]
+
+      matrix.append([folder, predict])
+
+      if predict != folder:
+          failed = failed +1
+  
+  print("Class " + folder + ": Total vali: " + str(len(probas)) + ", failed: " + str(failed))
+
+np.save("true-predict", np.array(matrix))
 
 #test_img = vali_datagen.
 #y_prob = model.predict(np.array( [test_img] ))
